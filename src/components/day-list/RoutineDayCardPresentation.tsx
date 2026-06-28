af8ad0ca91d69061ff8b9f4772e4daa82ca93b27
fawxzzy <fawxzzy@gmail.com>
2026-06-28 19:40:42 -0400

    Tighten routine day card top spacing

--- src/components/day-list/RoutineDayCardPresentation.tsx
+++ src/components/day-list/RoutineDayCardPresentation.tsx
@@ -19,7 +19,7 @@ import {
 
 export const ROUTINE_DAY_CARD_BODY_CLASS_NAME = "!min-h-[2.35rem] !py-[0.2rem]";
 export const ROUTINE_REST_DAY_CARD_BODY_CLASS_NAME = "!min-h-[3.2rem] !py-[0.24rem]";
-export const ROUTINE_DAY_CARD_CONTENT_CLASS_NAME = "!space-y-0 !pt-[0.45rem] !pb-0";
+export const ROUTINE_DAY_CARD_CONTENT_CLASS_NAME = "!space-y-0 !pt-[0.6rem] !pb-0";
 export const ROUTINE_REST_DAY_CARD_CONTENT_CLASS_NAME = "!min-h-0 py-0 !space-y-0";
 export const ROUTINE_DAY_CARD_SUBTITLE_CLASS_NAME = "text-[11.5px] leading-[1.14]";
 export const ROUTINE_DAY_CARD_TITLE_CLASS_NAME = "leading-[1.04]";
